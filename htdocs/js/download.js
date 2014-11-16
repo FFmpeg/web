21133ad758fe53f9aa4a5e78751c4db95b979291
Timothy Gu <timothygu99@gmail.com>
2014-11-15 21:26:10 -0800

    web/download.js: Detab
    Also fix the indentation of a line.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- htdocs/js/download.js
+++ htdocs/js/download.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function() {
-	$('#build-windows').removeClass('active');
-	$('#build-mac').removeClass('active');
+    $('#build-windows').removeClass('active');
+    $('#build-mac').removeClass('active');
 
-	var f = function (e) {
-	    e.preventDefault()
-	    $(this).tab('show')
-	};
-	$('a[href="#build-linux"]').hover(f);
-	$('a[href="#build-windows"]').hover(f);
-	$('a[href="#build-mac"]').hover(f);
+    var f = function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    };
+    $('a[href="#build-linux"]').hover(f);
+    $('a[href="#build-windows"]').hover(f);
+    $('a[href="#build-mac"]').hover(f);
 
-	$("#get-sources").height($("#get-packages").height());
-	$("#get-packages").height($("#get-packages").height());
-    });
+    $("#get-sources").height($("#get-packages").height());
+    $("#get-packages").height($("#get-packages").height());
+});
