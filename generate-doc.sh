5e6ad7e618b59b6500ae171ee5f8297f2aa227f9
Timothy Gu <timothygu99@gmail.com>
2014-12-03 21:31:33 -0800

    web/style.less: Styles for heading anchors
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- generate-doc.sh
+++ generate-doc.sh
@@ -30,6 +30,7 @@ export FFMPEG_HEADER1="$(cat src/template_head1)"
 export FFMPEG_HEADER2="$(cat src/template_head_prod src/template_head2)"
 export FFMPEG_HEADER3="$(cat src/template_head3)"
 export FFMPEG_FOOTER="$(cat src/template_footer1 src/template_footer_prod src/template_footer2)"
+export FA_ICONS=true
 
 rm -rf build-doc
 mkdir build-doc && cd build-doc
