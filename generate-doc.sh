ef2235cb94c534de54baefa24eca7f57f5f5f2c6
Daniel Verkamp <daniel@drv.nu>
2026-07-27 23:14:15 -0700

    web: drop --disable-yasm from generate-doc.sh
    This was removed in FFmpeg commit 2f888fb99eef ("configure:
    drop yasm support") and prevents configure from succeeding when running
    generate-doc.sh from the web tree.
    
    Signed-off-by: Daniel Verkamp <daniel@drv.nu>

--- generate-doc.sh
+++ generate-doc.sh
@@ -34,7 +34,7 @@ export FA_ICONS=true
 
 rm -rf build-doc
 mkdir build-doc && cd build-doc
-$src/configure --enable-gpl --disable-yasm || die "configure failed"
+$src/configure --enable-gpl || die "configure failed"
 make doc || die "doc not made"
 cp doc/*.html ../htdocs/ || die "copy failed"
 
